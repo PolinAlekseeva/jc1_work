21a022958248ef6237fa4009873edaf51760f70c
PolinAlekseeva <b.poline@gmail.com>
2022-02-22 21:12:39 +0300

    Added git ignore file

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,34 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		System.out.println();
 		System.out.println("Hello World!");
+		Car myCar;
+		myCar = new Car();
+		myCar.setPrice(20_000_000);
+		System.out.println(myCar.getPrice());
+		System.out.println(myCar.getModel().length());
 
+
+	}
+}
+
+class Car {
+
+	int price;
+	String model = "BMW X5";
+
+	public int getPrice() {
+		return price;
+
+	}
+
+	public void setPrice(int newPrice){
+
+
+	}
+
+	public String getModel(){
+		return model;
 	}
 }
